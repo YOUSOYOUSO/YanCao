00e629de097c667e986d60ce5428460e73bac981
d p <liuzhendong_GZ@outlook.com>
2020-02-13 18:41:12 +0800

    删除原本的版本

--- src/main/java/gcsj/repository/HuaXueRepository.java
+++
@@ -1,8 +0,0 @@
-package gcsj.repository;
-
-import gcsj.entity.HuaXue;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HuaXueRepository extends JpaRepository<HuaXue, Long> {
-
-}
--- src/main/java/gcsj/repository/PingXiRepository.java
+++
@@ -1,7 +0,0 @@
-package gcsj.repository;
-
-import gcsj.entity.PingXi;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PingXiRepository extends JpaRepository<PingXi, Long> {
-}
